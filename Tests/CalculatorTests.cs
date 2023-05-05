7aed54e6b7b9d35a77b15be2ba4ef479f2bd1af8
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-05 09:20:26 +0300

    chore: add tests for divide operation

--- Tests/CalculatorTests.cs
+++ Tests/CalculatorTests.cs
@@ -62,5 +62,32 @@ public class CalculatorTests
         result.Should().Be(expected);
     }
     
-    // TODO: finish tests for division
+    [Theory]
+    [InlineData(1, 2, 0.5)]
+    [InlineData(2, 1, 2)]
+    [InlineData(1, 3, 0.333333)]
+    [InlineData(2.67, 1.35, 1.97777778)]
+    [InlineData(-2.56, 1, -2.56)]
+    [InlineData(2.78, -7.89, -0.35234474)]
+    public void Divide_ShouldReturnExpected(double a, double b, double expected)
+    {
+        var result = _calculator.Divide(a, b);
+
+        result.Should().BeApproximately(expected, 1e-4);
+    }
+
+    [Theory]
+    [InlineData(1, 0)]
+    [InlineData(1.5, 0)]
+    [InlineData(0, 0)]
+    [InlineData(1, 1e-7)]
+    [InlineData(1.5, 1e-7)]
+    [InlineData(-1, 0)]
+    [InlineData(-1, 1e-7)]
+    public void Divide_ShouldThrowException_WhenDividerLessThatEpsilon(double a, double b)
+    {
+        var act = () => _calculator.Divide(a, b);
+
+        act.Should().ThrowExactly<DivideByZeroException>();
+    }
 }
