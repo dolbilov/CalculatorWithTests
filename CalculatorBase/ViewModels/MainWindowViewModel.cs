48e036680be1eb326ee75bdd26057d0a927a0f78
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-05 09:52:29 +0300

    chore: setup view model

--- CalculatorBase/ViewModels/MainWindowViewModel.cs
+++ CalculatorBase/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,63 @@
-﻿namespace CalculatorBase.ViewModels;
+﻿using System;
+using System.Collections;
+using CalculatorBase.Models;
+using ReactiveUI;
+
+namespace CalculatorBase.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    #region Fields
 
+    private string _firstArgument = string.Empty;
+    private string _secondArgument = string.Empty;
+    private Operation _selectedOperation;
+    private string _result = string.Empty;
+
+    #endregion
+
+    public MainWindowViewModel()
+    {
+        
+    }
+
+    #region Properties
+
+    public IEnumerable AvailableOperations => Enum.GetValues<Operation>();
+
+    public string FirstArgument
+    {
+        get => _firstArgument;
+        set => this.RaiseAndSetIfChanged(ref _firstArgument, value);
+    }
+
+    public string SecondArgument
+    {
+        get => _secondArgument;
+        set => this.RaiseAndSetIfChanged(ref _secondArgument, value);
+    }
+
+    public Operation SelectedOperation
+    {
+        get => _selectedOperation;
+        set => this.RaiseAndSetIfChanged(ref _selectedOperation, value);
+    }
+
+    public string Result
+    {
+        get => _result;
+        set => this.RaiseAndSetIfChanged(ref _result, value);
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    private void DoCalculations()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
 }
